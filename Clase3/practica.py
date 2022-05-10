905145423c70f96341d8d9e8ab0cda4cd94098f3
MaverickSeal <mikewasauski23@gmail.com>
2022-05-09 20:48:54 -0500

    ..

--- Clase3/practica.py
+++ Clase3/practica.py
@@ -4,7 +4,7 @@ if grados_centigrados >35:
     print ("ta juerte la calor")
 elif grados_centigrados >=25 and  grados_centigrados <=30 :
     print ("se antoja un pozol")
-elif grados_centigrados <25:
+elif grados_centigrados <=25:
     print ("es hora de sacar el sueter cucarachiento")
 else :
     print ("No se como esta la tempratura")
