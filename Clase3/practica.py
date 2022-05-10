77ad48a9e457d6a8337caa8c4678c47b3cbe3268
MaverickSeal <mikewasauski23@gmail.com>
2022-05-09 20:42:37 -0500

    listo

---
+++ Clase3/practica.py
@@ -0,0 +1,10 @@
+grados_centigrados = 30
+
+if grados_centigrados >35:
+    print ("ta juerte la calor")
+elif grados_centigrados >=25 and  grados_centigrados <=30 :
+    print ("se antoja un pozol")
+elif grados_centigrados <25:
+    print ("es hora de sacar el sueter cucarachiento")
+else :
+    print ("No se como esta la tempratura")
